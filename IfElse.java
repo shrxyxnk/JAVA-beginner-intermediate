73fe2344abbb608c1c698913ea20f711c2be2c7f
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-18 22:18:52 +0530

    IfElse.java
    A simple demo of the working of If-Else.

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]) {
+      int year=2026;
+       if(year==2025){
+      System.out.println("the year is 2025");
+
+    } else {
+       System.out.println("the year is not 2025 all conditions failed");
+    }
+    }
+}
