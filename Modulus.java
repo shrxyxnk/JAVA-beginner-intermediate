f54dcc110e3716bd248dac696d0f6008f5d7bf60
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-18 22:05:00 +0530

    Modulus.java
    A simple program that prints the remainder when two integers are divided.

---
+++ Modulus.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+       int a=100, b=20,result;
+        result= a%b;
+       System.out.println("the sum of integer is " + result);
+    }
+}
