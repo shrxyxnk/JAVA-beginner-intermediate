21b8493966e8ac39ebd2041f8d54f6e6297ce068
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-18 21:59:55 +0530

    SumOfIntegers.java
    A simple program that adds two integers.

---
+++ SumOfIntegers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+       int a=10, b=20,result;
+        result= a+b;
+       System.out.println("the sum of integer is " + result);
+    }
+}
