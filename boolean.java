e2d6c5a715d06be55083a11c47798bf3997794fb
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-19 11:19:00 +0530

    boolean.java
    Demo of Boolean data type.

---
+++ boolean.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]) {
+        boolean isActive = false;
+        if(isActive==true){
+        System.out.println("the user is active");
+        }  else {
+
+            System.out.println("the user is inactive");
+        }
+    }
+}
