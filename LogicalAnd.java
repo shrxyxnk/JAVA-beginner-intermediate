9f4c3ab544360464ea12115c16053a2710490511
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-19 11:26:05 +0530

    LogicalAnd.java
    Demo of logical and operator in java.

---
+++ LogicalAnd.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String args[]) {
+        int score = 100;
+        boolean isActive = true;
+        if(score == 100 && isActive = true)
+        {
+        System.out.println("you are on the next level");
+        }
+        else{
+            System.out.println("YOU ARE AT SAME LeVEL")
+        }
+
+}
+
+}
