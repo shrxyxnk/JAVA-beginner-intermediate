803da90511f97cb8cf196d509a4e36c17958b4e3
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-19 23:34:53 +0530

    DoWhileLoop.java
    A simple program implementing Do-While-Loop in java.

---
+++ DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       int number=20;
+
+       do {
+           System.out.println(number);
+           number++;
+       }while(number<=30);
+
+    }
+}
+
