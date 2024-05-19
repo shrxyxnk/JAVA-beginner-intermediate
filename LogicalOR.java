69a5b1bc8fbcd02ad69072a96fa9e682ae878a45
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-19 11:31:55 +0530

    LogicalOR.java
    Demo working og logical OR.

---
+++ LogicalOR.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+    int num1=-5, num2=-6;
+    if(num1>0 || num2> 0){
+        System.out.println("the condition is true");
+    }
+else{
+    System.out.println("all condtitions failed");
+}
+}
+
+}
