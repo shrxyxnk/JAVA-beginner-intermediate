8d6d08cc833217b7840b79730e2942dc071e6987
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-20 22:30:28 +0530

    ParameterisedMethod.java

---
+++ ParameterisedMethod.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        sayHello( "Shreyank",24);
+
+    }
+    public static void sayHello(String name ,int age){
+        System.out.println("hello "+ name + " your age is "+ age);
+    }
+}
+
+
+
