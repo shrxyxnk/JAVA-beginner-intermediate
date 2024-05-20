a0046ad7ff0857b1d5e9cdc093acba146c467e07
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-20 22:13:28 +0530

    Method.java
    First time implementing a method in java.

---
+++ Method.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    addTwoNums();
+
+    }
+      public static void addTwoNums(){
+        int a=30, b=50;
+          System.out.println("the result is "+ (a+b));
+      }
+
+}
