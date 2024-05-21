9d84e907ac3b9dad524e411ca4f4846d598138b2
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-21 21:45:24 +0530

    Array.java

---
+++ Array.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int [] numbers = {3,5,6,7,8,9,12,34};
+        for(int number : numbers){
+            System.out.println(number);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
