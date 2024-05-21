f2071b7a1cf2b542e2697f74bef965cb3dcaa5ff
Shreyank Ratnala <167080951+shrxyxnk@users.noreply.github.com>
2024-05-21 21:47:02 +0530

    array2.java

---
+++ array2.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int [] numbers = {3,5,6,7,8,9,12,34};
+       
+            System.out.println(numbers[3]);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
